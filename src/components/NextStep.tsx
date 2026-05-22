f87f7242a1ca1e9702fed0dc7e05a688c799f2cb
Chabib N. <chabib.nr@gmail.com>
2026-05-23 00:16:30 +0700

    chore: cleanup styles

--- src/components/NextStep.tsx
+++ src/components/NextStep.tsx
@@ -12,20 +12,7 @@ export const NextStep = defineComponent(
               default: '24px 20px',
               '@media (width >= 1024px)': '32px',
             },
-            borderBottom: {
-              default: null,
-              '@media (width >= 1024px)': '1px solid var(--border)',
-            },
-            borderRight: {
-              default: 'none',
-              '@media (width >= 1024px)': '1px solid var(--border)',
-            },
-            flexBasis: '0',
-            flexGrow: '1',
-            flexShrink: '1',
-            borderRightColor: 'var(--border)',
-            borderRightStyle: 'solid',
-            borderRightWidth: '1px',
+            flex: '1',
           },
           props.style,
         )}
